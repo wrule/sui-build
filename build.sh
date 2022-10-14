f6751611fe59cb217d2eccf3a74260bea79fccf1
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-14 14:15:26 +0800

    提交

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 apt update
 apt install curl wget git-all pkg-config libssl-dev libclang-dev llvm clang
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup update stable
+cargo install --locked --git https://github.com/MystenLabs/sui.git --branch "devnet" sui sui-gateway
